30e1e0d4d53e846501f3c3e7505f2ec8a81ceffb
sahernandezib <126585960+sahernandezib@users.noreply.github.com>
2023-05-25 21:31:28 -0500

    Update pregunta_04.sql

--- pregunta_04.sql
+++ pregunta_04.sql
@@ -39,4 +39,5 @@
 --  2  C  CCCE
 --
 --  >>> Escriba su codigo a partir de este punto <<<
--- SELECT k0, c16 FROM tbl1 WHERE SUBSTR(k0,1,1) = SUBSTR(c16,1,1)
+-- 
+SELECT k0,c16 FROM tbl1 t WHERE SUBSTR(c16,1,1)=K0;
